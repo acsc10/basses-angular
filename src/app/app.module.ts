d7449582f8b8e965c4dae4eb374b91f2a8c23a3a
AndresSanchezC <andressanchez@elecsis.co>
2024-08-09 16:44:52 -0500

    first commit

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -3,14 +3,18 @@ import { BrowserModule } from '@angular/platform-browser';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
+import { CourterModule } from './counter/counter.module';
+import { HeroesModule } from './heroes/heroes.module';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
   ],
   imports: [
     BrowserModule,
-    AppRoutingModule
+    AppRoutingModule,
+    CourterModule,
+    HeroesModule
   ],
   providers: [],
   bootstrap: [AppComponent]
